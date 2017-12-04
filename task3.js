4aee735515792e11dcfdaa3a3672fa772afcaad4
dmrock <equi.denis@gmail.com>
2017-12-04 14:18:05 +0300

    task 3

---
+++ task3.js
@@ -0,0 +1,34 @@
+var webdriver = require('selenium-webdriver'),
+    chrome = require('selenium-webdriver/chrome'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
+
+test.describe('Open admin page, login as admin', function() {
+    var driver;
+
+    test.before(function() {
+        var options = new chrome.Options();
+        options.addArguments(["start-fullscreen"]);
+
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(options)
+            .build();
+        driver.getCapabilities().then(function(caps) {
+            console.log(caps);
+        });
+    });
+
+    test.it('should open admin page and login as admin', function() {
+        driver.get('http://localhost:8888/litecart/admin');
+        driver.findElement(By.name('username')).sendKeys('admin');
+        driver.findElement(By.name('password')).sendKeys('admin');
+        driver.findElement(By.name('login')).click();
+        driver.wait(until.titleIs('My Store'), 1000);
+    });
+
+    test.after(function() {
+        driver.quit();
+    });
+});
